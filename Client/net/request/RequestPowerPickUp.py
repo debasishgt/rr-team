57a85dc7bac7fc67c5229bd59eec771480972dfc
Vicken52 <krikorian.vicken@gmail.com>
2015-11-17 21:55:59 -0800

    Added last of Client Protocols

---
+++ Client/net/request/RequestPowerPickUp.py
@@ -0,0 +1,20 @@
+from direct.distributed.PyDatagram import PyDatagram
+from common.Constants import Constants
+from net.request.ServerRequest import ServerRequest
+
+class RequestPowerPickUp(ServerRequest):
+
+  #args stores the x y z components of the moving
+    def send(self, args = []):
+
+        try:
+            pkg = PyDatagram()
+            pkg.addUint16(Constants.CMSG_POWER_UP_PICK_UP)
+            pkg.addInt32(args[0])
+
+            self.cWriter.send(pkg, self.connection)
+
+            #print("sent move: ", args[0], " ",args[1], " ", args[2])
+            #self.log('Sent [' + str(Constants.RAND_FLOAT) + '] Float Request')
+        except:
+            self.log('Bad [' + str(Constants.CMSG_MOVE) + '] Move Request')
---
+++ Client/net/response/ResponsePowerUp.py
@@ -0,0 +1,26 @@
+from traceback import print_exc
+
+from common.Constants import Constants
+from net.response.ServerResponse import ServerResponse
+
+class ResponsePowerUp(ServerResponse):
+
+    def execute(self, data):
+
+        try:
+            self.playerId = data.getInt32()
+            self.powerId = data.getInt32()
+
+
+            # Need to set Heading and use keys
+            for character in self.world.characters :
+              if character.playerId == self.playerId :
+                # Sends a visual effect from the character model
+                # Also hides the powerId from the map until it respawns.
+                break
+                
+            #self.log('Received [' + str(Constants.RAND_STRING) + '] String Response')
+
+        except:
+            self.log('Bad [' + str(Constants.RAND_STRING) + '] String Response')
+            print_exc()
