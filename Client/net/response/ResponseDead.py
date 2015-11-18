716457a2633a458bcf502165d3e6d55d07a13c13
Vicken52 <krikorian.vicken@gmail.com>
2015-11-17 21:01:11 -0800

    Added Collision, Dead, and Health Protocols (Incomplete)

---
+++ Client/net/response/ResponseDead.py
@@ -0,0 +1,24 @@
+from traceback import print_exc
+
+from common.Constants import Constants
+from net.response.ServerResponse import ServerResponse
+
+class ResponseMove(ServerResponse):
+
+    def execute(self, data):
+
+        try:
+            self.playerId = data.getInt32()
+
+
+            # Need to have a variable checking if the character is alive
+            for character in self.world.characters :
+              if character.playerId == self.playerId :
+                #character.dead = True
+
+            print "ResponseMove - ",self.playerId
+            #self.log('Received [' + str(Constants.RAND_STRING) + '] String Response')
+
+        except:
+            self.log('Bad [' + str(Constants.RAND_STRING) + '] String Response')
+            print_exc()
