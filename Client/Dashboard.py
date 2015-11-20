1d1bd55a33180f2d5185ea56f568a16810b23c48
c1rabbit <mail@calvinthanh.com>
2015-11-19 17:01:39 -0800

    updated

--- Client/Dashboard.py
+++ Client/Dashboard.py
@@ -7,10 +7,13 @@ import datetime, time
 
 class Dashboard(DirectObject):
     def __init__(self, character, taskMgr):
+        self.total_players = 1
+        self.rank = 1
         self.main_char = character
         self.speed = "0.0 km/h"
         self.start_time = datetime.datetime.now()
         self.time_elapsed = datetime.timedelta(milliseconds=0)
+        # insert total time
         self.game_time = datetime.timedelta(minutes=8) - self.time_elapsed
         # print self.game_time
 
@@ -23,15 +26,26 @@ class Dashboard(DirectObject):
                      scale=.07)
 
         # Rank
-        OnscreenText(text="2", style=1, fg=(1, 1, 1, 1),
-                     pos=(-.95, .85), align=TextNode.ALeft,
-                     scale=.15)
+        self.display_rank = OnscreenText(text=str(self.rank), style=1, fg=(1, 1, 1, 1),
+                                         pos=(-.8, .85), align=TextNode.ALeft,
+                                         scale=.15)
         OnscreenText(text="/", style=1, fg=(1, 1, 1, 1),
-                     pos=(-.85, .85), align=TextNode.ALeft,
+                     pos=(-.7, .85), align=TextNode.ALeft,
                      scale=.18)
-        OnscreenText(text="5", style=1, fg=(1, 1, 1, 1),
-                     pos=(-.75, .85), align=TextNode.ALeft,
-                     scale=.15)
+        self.display_players = OnscreenText(text=str(self.total_players), style=1, fg=(1, 1, 1, 1),
+                                            pos=(-.6, .85), align=TextNode.ALeft,
+                                            scale=.15)
+
+        # Ranking
+        OnscreenText(text="1st", style=1, fg=(1, 1, 1, 1),
+                     pos=(-1.3, .5), align=TextNode.ALeft,
+                     scale=.05)
+        OnscreenText(text="2nd", style=1, fg=(1, 1, 1, 1),
+                     pos=(-1.3, .45), align=TextNode.ALeft,
+                     scale=.05)
+        OnscreenText(text="3rd", style=1, fg=(1, 1, 1, 1),
+                     pos=(-1.3, .4), align=TextNode.ALeft,
+                     scale=.05)
 
         # Powerups
         OnscreenText(text="1", style=1, fg=(1, 1, 1, 1),
@@ -50,6 +64,7 @@ class Dashboard(DirectObject):
 
         taskMgr.doMethodLater(.1, self.show_speed, 'updateSpeed')
         taskMgr.doMethodLater(.1, self.show_timer, 'updateTimer')
+        taskMgr.doMethodLater(.1, self.update_rank, 'updateRank')
 
     def show_speed(self, task):
         self.speed = str(format(self.main_char.vehicle.getCurrentSpeedKmHour(), '0.2f')) + "km/h"
@@ -66,4 +81,21 @@ class Dashboard(DirectObject):
         game_time = str(datetime.timedelta(minutes=8) - self.time_elapsed)[2:11]
         self.display_timer.destroy()
         self.display_timer = OnscreenText(text=game_time, style=3, fg=(1, 1, 1, 1), pos=(0, .9), scale=.1)
+        return task.cont
+
+    # server updates client time in ms
+    def force_timer(self, server_time):
+        self.time_elapsed = datetime.timedelta(milliseconds=server_time)
+
+    def update_rank(self, task):
+        self.display_players.destroy()
+        self.display_rank.destroy()
+
+        # Rank
+        self.display_rank = OnscreenText(text=str(self.rank), style=1, fg=(1, 1, 1, 1),
+                                         pos=(-.8, .85), align=TextNode.ALeft,
+                                         scale=.15)
+        self.display_players = OnscreenText(text=str(self.total_players), style=1, fg=(1, 1, 1, 1),
+                                            pos=(-.6, .85), align=TextNode.ALeft,
+                                            scale=.15)
         return task.cont
